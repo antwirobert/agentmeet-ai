75840d99d5251b8621c8f2495fab27fc587d696c
Robert Antwi <thename3438@gmail.com>
2025-11-24 18:41:42 +0000

    add padding below pagination

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -21,7 +21,7 @@ export const MeetingsView = () => {
   )
 
   return (
-    <div className="w-full pb-5">
+    <div className="w-full pb-10">
       <DataTable
         columns={columns}
         data={data.meetings}
